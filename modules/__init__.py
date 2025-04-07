bf1359644175410ffd86cca95fa1e7a3f7549618
Subha Subbiah <subha@Subhas-MacBook-Air.local>
2025-04-07 13:32:14 +0530

    filenames changed

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,4 @@
-from .app_APK import app_APK
-from .app_sub import app_sub
-from .app_pr import app_pr
+from .app_bs import app_APK
+from .app_analytics import app_sub
+from .app_shorten import app_pr
 from .app_admin import app_admin
