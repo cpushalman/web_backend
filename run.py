72ff2c1ca0e4095e17f98e455877a1b89b415125
Preethi <24z256@psgtech.ac.in>
2025-05-07 13:43:49 +0000

    The ports can now be different

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 # run.py
-
+import os
+from dotenv import load_dotenv
 from core.main_app import MainApp
 
-
+load_dotenv()
 
 if __name__ == "__main__":
     main_app = MainApp()
     app = main_app.get_app()
+    port = int(os.getenv("PORT", 5000))
     app.run(debug=True)    
