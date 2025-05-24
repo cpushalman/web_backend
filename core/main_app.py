4fbb634e50daadd6b5d2158fc42c4d10b3fbbbda
Arrvindh PK <arrvindhpk@gmail.com>
2025-05-24 18:40:15 +0530

    Update main_app.py

--- core/main_app.py
+++ core/main_app.py
@@ -9,17 +9,6 @@ import os
 from pymongo import MongoClient, errors
 from flask_cors import CORS
 
-from flask_jwt_extended import JWTManager
-from app.user_management.routes import auth
-
-app = Flask(__name__)
-app.config["JWT_SECRET_KEY"] = "your-secret-key"
-jwt = JWTManager(app)
-
-app.register_blueprint(auth, url_prefix="/auth")
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 
