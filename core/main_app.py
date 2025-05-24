3912bd8abb1ebbc45afcdc5636ad284b9359a247
SHALMAN <shalman4502n@gmail.com>
2025-05-24 20:47:47 +0530

    ready to merge

--- core/main_app.py
+++ core/main_app.py
@@ -10,7 +10,7 @@ from pymongo import MongoClient, errors
 from flask_cors import CORS
 
 from flask_jwt_extended import JWTManager
-from user_management.routes import auth
+from modules.auth import auth
 
 
 class MainApp:
