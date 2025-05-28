fe845566e6ea1a7fab978b25228eca233266a466
SHALMAN <shalman4502n@gmail.com>
2025-05-28 17:55:51 +0530

    render deployement testing

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 import os
 from dotenv import load_dotenv
 from core.main_app import MainApp
+import sys
+
+sys.path.append("C:/Users/Ahamed shalman/Documents/GitHub/preethicodes/web_backend")
 
 load_dotenv()
 
@@ -11,3 +14,6 @@ app = main_app.get_app()
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     app.run(debug=True)
+
+
+    
